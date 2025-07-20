7fa927480ab8a5f40bc42f84adaca3f9d386267b
LagishettyNeha <lagishettyneha13@gmail.com>
2025-07-20 20:38:14 +0530

    students unable to eat

---
+++ numofstu_unable_to_eat_lunch.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        i = 0
+        while len(students) > i:
+            if students[0] == sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
+                i = 0
+            else:
+                students.append(students.pop(0))
+                i += 1
+        return len(students)
